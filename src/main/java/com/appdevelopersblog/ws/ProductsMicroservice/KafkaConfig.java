ea608d43540bc4a09b30a1e1752ad6f71d471a63
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-14 13:37:41 +0100

    commit

--- src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/KafkaConfig.java
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/KafkaConfig.java
@@ -1,18 +1,21 @@
 package com.appdevelopersblog.ws.ProductsMicroservice;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
 
+    @Bean
     NewTopic createTopic() {
 
-        return TopicBuilder.name("product-created-events-topic").build();
+        return TopicBuilder
+                .name("product-created-events-topic")
+                .partitions(3)
+                .replicas(1)
+                .config("min.insync.replicas", "2")
+                .build();
     }
-
-
-
 }
