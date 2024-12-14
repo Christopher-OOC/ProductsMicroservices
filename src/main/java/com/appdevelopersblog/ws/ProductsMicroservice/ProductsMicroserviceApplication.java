3c6043612158c28288e48be0f94cde5e589ddafe
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-14 13:29:06 +0100

    commit

---
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/ProductsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.appdevelopersblog.ws.ProductsMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMicroserviceApplication.class, args);
+	}
+
+}
