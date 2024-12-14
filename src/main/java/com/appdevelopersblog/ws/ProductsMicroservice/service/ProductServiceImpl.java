7cee36599bc481e34cc58a21034e361764fbb5f8
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-14 15:21:27 +0100

    commit

--- src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/service/ProductServiceImpl.java
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/service/ProductServiceImpl.java
@@ -1,13 +1,47 @@
 package com.appdevelopersblog.ws.ProductsMicroservice.service;
 
 import com.appdevelopersblog.ws.ProductsMicroservice.model.CreateProductRestModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductRestModel productRestModel) {
-        return "";
+
+        String productId = UUID.randomUUID().toString();
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(
+                productId,
+                productRestModel.getTitle(),
+                productRestModel.getPrice(),
+                productRestModel.getQuantity());
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future = kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((result, exception) -> {
+           if (exception != null) {
+                LOGGER.error();
+           }
+           else {
+
+           }
+        });
+
+        return productId;
     }
 }
