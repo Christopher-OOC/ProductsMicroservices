8fcd10202c9f9ed58f4a17eb310bf208fffef5e7
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-14 13:59:21 +0100

    commit

--- src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/controller/ProductController.java
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/controller/ProductController.java
@@ -1,6 +1,21 @@
 package com.appdevelopersblog.ws.ProductsMicroservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @PostMapping
+    public ResponseEntity<String> createProduct() {
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
+    }
+
 
 
 
