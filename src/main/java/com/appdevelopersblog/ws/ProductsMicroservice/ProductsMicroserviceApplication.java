08360b238e161c488a0c70a8076d20af0689f08b
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-15 18:01:55 +0100

    commit

--- src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/ProductsMicroserviceApplication.java
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/ProductsMicroserviceApplication.java
@@ -3,6 +3,8 @@ package com.appdevelopersblog.ws.ProductsMicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
 @SpringBootApplication
 public class ProductsMicroserviceApplication {
 
